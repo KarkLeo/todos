6de45a42b446b03bd6b010cc449ba561795ed606
Vyacheslav <v.malykhin.ff@gmail.com>
2020-12-08 17:26:20 +0200

    go to mogoDB

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         uri: env('DATABASE_URI'),
-        database: 'todos-dev'
+        database: env('DATABASE_NAME')
       },
       options: {
         ssl: true,
